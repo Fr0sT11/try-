d7dc1c51d1723f3515416239e39e64be2a0efc10
unknown <abiskartimsina4mini@gmail.com>
2020-05-26 12:32:00 +0545

    Inital Commit

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+DATA_BUFFER = 8
+PORT = 1234
+IP_ADDRESS = "192.168.1.113"
+ADDRESS = (IP_ADDRESS,PORT)
+FORMAT = 'utf-8'
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDRESS)
+
+def send(message):
+	message = message.encode(FORMAT)
+	msg_length = len(message)
+	msg_length = str(msg_length).encode(FORMAT)
+	msg_length += b' '*(DATA_BUFFER - len(msg_length))
+	client.send(msg_length)
+	client.send(message)
+
+def receive():
+	while 1:
+		message = client.recv(DATA_BUFFER).decode(FORMAT)
+		
+		if message != 0:
+			try:
+				message_length = int(message)
+			except:
+				continue
+				
+			msg = client.recv(message_length).decode(FORMAT)
+			print (f'\n {msg}')
+		else:
+			pass
+
+
+thread = threading.Thread(target=receive)
+thread.start()
+
+while 1:
+	msg = input()
+	send(msg)
